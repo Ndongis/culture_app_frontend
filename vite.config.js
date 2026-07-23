9bb07317eb482f84e867c9a0fdcf76ad40c2cf4a
Ndongis <ndongisecka@gmail.com>
2026-07-23 15:01:19 +0000

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
-
+import React from 'react';
 export default defineConfig({
   plugins: [
     react(),
